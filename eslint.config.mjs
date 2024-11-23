a83f256c5051f874c818dd6832387048e79b5539
Vladislav <vlaaadislav@gmail.com>
2024-11-23 18:13:52 +0300

    typechek

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   rules: {
     'vue/no-deprecated-slot-attribute': 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: 1,
+    }],
   },
 })
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
-import HomePage from '@/views/HomePage.vue'
+import HomePage from '@/views/WordsList.vue'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 
 describe('homePage.vue', () => {
   it('renders home vue', () => {
     const wrapper = mount(HomePage)
-    expect(wrapper.text()).toMatch('Ready to create an app?')
+    expect(wrapper.text()).toMatch('Words List')
   })
 })
