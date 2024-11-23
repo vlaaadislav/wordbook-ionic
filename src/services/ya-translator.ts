953a2765487769e1ad777e90f33fa135f2eb10e3
Vladislav <vlaaadislav@gmail.com>
2024-11-23 18:44:55 +0300

    hide token

--- src/services/ya-translator.ts
+++ src/services/ya-translator.ts
@@ -11,10 +11,10 @@ interface DictionaryResponse {
 }
 
 const DICT = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup'
-const DICT_KEY = 'dict.1.1.20190113T173518Z.6b5f6fc6886f7a00.04b3ef123e6617a253f041967b509d7e6d352ba9'
+const DICT_KEY = import.meta.env.VITE_YANDEX_DICT_KEY
 
 const TRANSLATE = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-const TRANSLATE_KEY = 'trnsl.1.1.20190113T172305Z.c4f9164143be1338.970bea30a1fd7e8bfd561e1e7acba2073898b198'
+const TRANSLATE_KEY = import.meta.env.VITE_YANDEX_TRANSLATE_KEY
 
 async function doRequest(...args: Parameters<typeof fetch>): ReturnType<typeof fetch> {
   const response = await fetch(...args)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_YANDEX_DICT_KEY: string
+  readonly VITE_YANDEX_TRANSLATE_KEY: string
+}
